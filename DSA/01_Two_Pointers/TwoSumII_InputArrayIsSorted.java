f6194b8851322a39d6986d6593ef70726637e065
Siddharth Kumar <161417346+siddharth0161820@users.noreply.github.com>
2025-05-31 14:36:25 +0530

    Add LeetCode solution: Two Sum II - Input Array Is Sorted

---
+++ DSA/01_Two_Pointers/TwoSumII_InputArrayIsSorted.java
@@ -0,0 +1,18 @@
+//Time Complexity=O(n)
+class Solution {
+    public int[] twoSum(int[] arr, int target) {
+        //TWO POINTER APPROACH
+        int i=0;
+        int j=arr.length-1;
+        while(i<j){
+                 if(arr[i]+arr[j]>target){
+                     j--;
+                 }else if(arr[i]+arr[j]<target){
+                       i++;
+                 } else{
+                    return new int[]{i+1,j+1};
+                 }
+        }
+        return null;
+    }
+}
